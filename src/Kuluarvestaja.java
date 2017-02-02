ac53fd7d706235bb5bb77169b200bed4ea567a57
Rudolf112 <rudolfpurge112@gmail.com>
2017-02-02 14:23:07 +0200

    kuluarvestaja valmis

--- src/Kuluarvestaja.java
+++ src/Kuluarvestaja.java
@@ -1,6 +1,56 @@
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by rudolf on 1.02.2017.
  */
 public class Kuluarvestaja {
     int palk;
-}
+    int suurimaKuluIndeks = 0;
+    int suurimKulu = 0;
+    int kuludKokku = 0;
+
+    ArrayList<String> kulunimi = new ArrayList<>();
+    ArrayList<String> kulusuurus = new ArrayList<>();
+
+    Kuluarvestaja(){}
+
+    void setPalk(int palk){
+        this.palk = palk;
+    }
+    String sisestaKulu(String nimi, String suurus) {
+        if (Integer.parseInt(suurus) > palk) {
+            return "Kulu on suurem kui allesjäänud palk!";
+        } else {
+            kulunimi.add(nimi);
+            kulusuurus.add(suurus);
+            palk -= Integer.parseInt(suurus);
+            return "Kulu sisestatud!";
+        }
+    }
+
+    String kulu() {
+        kuludKokku = 0;
+        suurimaKuluIndeks = 0;
+        for (int i = 0; i < kulunimi.size(); i++) {
+            kuludKokku += Integer.parseInt(kulusuurus.get(i));
+            if (Integer.parseInt(kulusuurus.get(i)) > suurimKulu) {
+                suurimaKuluIndeks = i;
+                suurimKulu = Integer.parseInt(kulusuurus.get(i));
+            }
+        }
+        String suurimKuluString = ("Suurim kulu seni: " + kulunimi.get(suurimaKuluIndeks) + " " + Integer.toString(suurimKulu) + ". Kulud kokku: " + kuludKokku);
+        System.out.println(kulunimi);
+        return suurimKuluString;
+
+    }
+    void nulliAndmed(){
+        kulunimi.clear();
+        kulusuurus.clear();
+    }
+    String pakuLoobumist(){
+        int indeks = ThreadLocalRandom.current().nextInt(0, kulunimi.size());
+        return "Kas äkki saaks loobuda: " + kulunimi.get(indeks) + kulusuurus.get(indeks);
+    }
+    }
+
--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,58 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Pane pane = new Pane();
         Scene scene = new Scene(pane, 500, 500);
+        Kuluarvestaja k1 = new Kuluarvestaja();
+        Label juhis = new Label("Sisesta lahtrisse palk");
 
-        Label juhis = new Label("Sisesta andmed");
+        TextField palk = new TextField();
+        palk.setTranslateY(30);
 
+        Button submit = new Button("Salvesta palk");
+        submit.setTranslateY(60);
+
+        Label juhis2 = new Label("Sisesta kulu nimetus");
+        juhis2.setTranslateY(100);
+
+        TextField kulu = new TextField();
+        kulu.setTranslateY(130);
+
+        Label juhis3 = new Label("Sisesta kulu suurus");
+        juhis3.setTranslateY(160);
+
+        TextField kulu2 = new TextField();
+        kulu2.setTranslateY(190);
+
+        Button submit2 = new Button("Sisesta kulu");
+        submit2.setTranslateY(220);
+
+        Label suurimkulu = new Label();
+        suurimkulu.setTranslateY(250);
+
+        Label info = new Label();
+        info.setTranslateY(280);
+
+        Label loobumine = new Label();
+        loobumine.setTranslateY(310);
+
+        pane.getChildren().addAll(juhis, palk, submit, juhis2, kulu, submit2, kulu2, juhis3, suurimkulu, info, loobumine);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        submit.setOnMouseClicked(event -> {
+            k1.nulliAndmed();
+            info.setText("Uus palk salvestatud!");
+            suurimkulu.setText("");
+            loobumine.setText("");
+            k1.setPalk(Integer.parseInt(palk.getText()));
+        });
+
+        submit2.setOnMouseClicked(event -> {
+            info.setText(k1.sisestaKulu(kulu.getText(), kulu2.getText()));
+            suurimkulu.setText(k1.kulu());
+            loobumine.setText(k1.pakuLoobumist());
+            kulu.clear();
+            kulu2.clear();
+        });
 
     }
 }
